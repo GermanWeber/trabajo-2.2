54cf33b768f256750fbbfddbc6cd53245cc0dd85
German <ge.weber@duocuc.cl>
2024-06-01 20:01:19 -0400

    primer

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+
+
+
+const fetchApiAll = async () => {
+    try {
+        
+        const response = await fetch(`https://api-de-zapatos.onrender.com`);
+        const data = await response.json();
+        console.log(data);
+        return data.results;
+        
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+fetchApiAll();
